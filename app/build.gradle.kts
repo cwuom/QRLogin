e55a76f243d715f6ce9525c58594e32e60d125b6
RJMultiDev <RJMultiDev@github.com>
2025-10-04 01:24:45 +0000

    [skip ci]little change@1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "qq.qrlogin"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "qq.qrlogin"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
