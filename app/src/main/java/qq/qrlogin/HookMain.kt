fffc313614b99b3bc86f90434fc957d33a2c4d0c
RJMultiDev <RJMultiDev@github.com>
2025-10-04 01:33:09 +0000

    little change@2

--- app/src/main/java/qq/qrlogin/HookMain.kt
+++ app/src/main/java/qq/qrlogin/HookMain.kt
@@ -1,87 +1,89 @@
-package qq.qrlogin;
+package qq.qrlogin
 
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import qq.qrlogin.util.Logger;
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+import qq.qrlogin.util.Logger
 
-public class HookMain implements IXposedHookLoadPackage {
+class HookMain : IXposedHookLoadPackage {
 
-    private final String jsToInject =
-            "(function() {\n" +
-                    "    if (window.hasInjectedFinal) return;\n" +
-                    "    window.hasInjectedFinal = true;\n" +
-                    "    console.log('[QRLogin_JS_Final] Final hook script started.');\n" +
-                    "    var responseTextDescriptor = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, 'responseText');\n" +
-                    "    Object.defineProperty(XMLHttpRequest.prototype, 'responseText', {\n" +
-                    "        get: function() {\n" +
-                    "            var url = this.responseURL || '';\n" +
-                    "            if (url.includes('queryloginverifymethod')) {\n" +
-                    "                console.log('[QRLogin_JS_Final] Intercepting responseText for: ' + url);\n" +
-                    "                var fakeJson = '{\"retcode\":0,\"retmsg\":\"\",\"phoneVerify\":false,\"faceVerify\":true,\"userinfoVerify\":false,\"verifyInfoMap\":0,\"passwordVerify\":false,\"mbQAVerify\":false,\"getSupportUrlRsp\":\"\",\"uinCompletionVerify\":false,\"qrCodeVerify\":true,\"realnameVerify\":false,\"appeal\":0,\"guaranteeVerify\":false,\"robotVerify\":false,\"wechatVerify\":false,\"wechatNickname\":\"\"}';\n" +
-                    "                return fakeJson;\n" +
-                    "            }\n" +
-                    "            return responseTextDescriptor.get.apply(this, arguments);\n" +
-                    "        },\n" +
-                    "        configurable: true\n" +
-                    "    });\n" +
-                    "    console.log('[QRLogin_JS_Final] XMLHttpRequest.responseText getter has been hooked.');\n" +
-                    "})();";
+    private val jsToInject = """
+        (function() {
+            if (window.hasInjectedFinal) return;
+            window.hasInjectedFinal = true;
+            console.log('[QRLogin_JS_Final] Final hook script started.');
+            var responseTextDescriptor = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, 'responseText');
+            Object.defineProperty(XMLHttpRequest.prototype, 'responseText', {
+                get: function() {
+                    var url = this.responseURL || '';
+                    if (url.includes('queryloginverifymethod')) {
+                        console.log('[QRLogin_JS_Final] Intercepting responseText for: ' + url);
+                        var fakeJson = '{"retcode":0,"retmsg":"","phoneVerify":false,"faceVerify":true,"userinfoVerify":false,"verifyInfoMap":0,"passwordVerify":false}';
+                        return fakeJson;
+                    }
+                    return responseTextDescriptor.get.apply(this, arguments);
+                },
+                configurable: true
+            });
+            console.log('[QRLogin_JS_Final] XMLHttpRequest.responseText getter has been hooked.');
+        })();
+    """.trimIndent()
 
+    override fun handleLoadPackage(lpparam: LoadPackageParam) {
+        if (lpparam.packageName != "com.tencent.mobileqq" || lpparam.processName != "com.tencent.mobileqq:tool") {
+            return
+        }
 
-    @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.tencent.mobileqq")) return;
-        if (!lpparam.processName.equals("com.tencent.mobileqq:tool")) return;
-
-        Logger.d("load >> " + lpparam.processName);
+        Logger.d("loading to ${lpparam.processName}")
 
         try {
-            final Class<?> webViewClientClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebViewClient", lpparam.classLoader);
-            final Class<?> webViewClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebView", lpparam.classLoader);
+            val webViewClientClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebViewClient", lpparam.classLoader)
+            val webViewClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebView", lpparam.classLoader)
+            val webChromeClientClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebChromeClient", lpparam.classLoader)
+            val consoleMessageClass = XposedHelpers.findClass("com.tencent.smtt.export.external.interfaces.ConsoleMessage", lpparam.classLoader)
 
             XposedHelpers.findAndHookMethod(
-                    webViewClientClass,
-                    "onPageFinished",
-                    webViewClass,
-                    String.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            Object webView = param.args[0];
-                            String url = (String) param.args[1];
+                webViewClientClass,
+                "onPageFinished",
+                webViewClass,
+                String::class.java,
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        val webView = param.args[0]
+                        val url = param.args[1] as String
 
-                            if (url != null && url.contains("accounts.qq.com/safe/verify")) {
-                                Logger.i("!!! onPageFinished >> " + url);
-                                XposedHelpers.callMethod(webView, "evaluateJavascript", jsToInject, null);
-                                Logger.i("Injection successful!");
-                            }
-                        }
-                    }
-            );
-
-            final Class<?> webChromeClientClass = XposedHelpers.findClass("com.tencent.smtt.sdk.WebChromeClient", lpparam.classLoader);
-            final Class<?> consoleMessageClass = XposedHelpers.findClass("com.tencent.smtt.export.external.interfaces.ConsoleMessage", lpparam.classLoader);
-            XposedHelpers.findAndHookMethod(webChromeClientClass, "onConsoleMessage", consoleMessageClass, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) {
-                    Object consoleMessage = param.args[0];
-                    if (consoleMessage != null) {
-                        String message = (String) XposedHelpers.callMethod(consoleMessage, "message");
-                        if (message.contains("QRLogin_JS_Final")) {
-                            Logger.e("[script]: " + message);
+                        if (url.contains("accounts.qq.com/safe/verify") == true) {
+                            Logger.i("!!! onPageFinished >> $url")
+                            XposedHelpers.callMethod(webView, "evaluateJavascript", jsToInject, null)
+                            Logger.i("Injection successful!")
                         }
                     }
                 }
-            });
+            )
 
-            Logger.d("all monitoring points have been set up");
+            XposedHelpers.findAndHookMethod(
+                webChromeClientClass,
+                "onConsoleMessage",
+                consoleMessageClass,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        param.args[0]!!.let { consoleMessage ->
+                            val message = XposedHelpers.callMethod(consoleMessage, "message") as String
+                            if (message.contains("QRLogin_JS_Final") == true) {
+                                Logger.e("[script]: $message")
+                            }
+                        }
+                    }
+                }
+            )
 
-        } catch (Throwable t) {
-            Logger.e("Hook failed: " + t.getMessage());
-            XposedBridge.log(t);
+            Logger.d("all monitoring points have been set up")
+
+        } catch (t: Throwable) {
+            Logger.e("Hook failed: ${t.message}")
+            XposedBridge.log(t)
         }
     }
 }
